e187b25d7918d460149a4def495d789df8a4d562
Edmundo Paulino <manyymoore@gmail.com>
2021-11-04 21:41:07 +0000

    test: ✅ Add tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,60 +1,96 @@
 // https://doc.rust-lang.org/std/primitive.u16.html#method.checked_sub
 
-const TABLE: [(&str, usize, usize); 30] = [
-    ("MMM", 3000, 2),
-    ("MM", 2000, 1),
-    ("M", 1000, 0),
-    ("CM", 900, 8),
-    ("DCCC", 800, 7),
-    ("DCC", 700, 6),
-    ("DC", 600, 5),
-    ("D", 500, 4),
-    ("CD", 400, 3),
-    ("CCC", 300, 2),
-    ("CC", 200, 1),
-    ("C", 100, 0),
-    ("XC", 90, 8),
-    ("LXXX", 80, 7),
-    ("LXX", 70, 6),
-    ("LX", 60, 5),
-    ("L", 50, 4),
-    ("XL", 40, 3),
-    ("XXX", 30, 2),
-    ("XX", 20, 1),
-    ("X", 10, 0),
-    ("IX", 9, 8),
-    ("VIII", 8, 7),
-    ("VII", 7, 6),
-    ("VI", 6, 5),
-    ("V", 5, 4),
-    ("IV", 4, 3),
-    ("III", 3, 2),
-    ("II", 2, 1),
-    ("I", 1, 0),
+const THOUSANDS: [(&str, usize); 3] = [("MMM", 3000), ("MM", 2000), ("M", 1000)];
+const HUNDREDS: [(&str, usize); 9] = [
+    ("CM", 900),
+    ("DCCC", 800),
+    ("DCC", 700),
+    ("DC", 600),
+    ("D", 500),
+    ("CD", 400),
+    ("CCC", 300),
+    ("CC", 200),
+    ("C", 100),
+];
+const TENS: [(&str, usize); 9] = [
+    ("XC", 90),
+    ("LXXX", 80),
+    ("LXX", 70),
+    ("LX", 60),
+    ("L", 50),
+    ("XL", 40),
+    ("XXX", 30),
+    ("XX", 20),
+    ("X", 10),
+];
+const UNITS: [(&str, usize); 9] = [
+    ("IX", 9),
+    ("VIII", 8),
+    ("VII", 7),
+    ("VI", 6),
+    ("V", 5),
+    ("IV", 4),
+    ("III", 3),
+    ("II", 2),
+    ("I", 1),
 ];
 
 pub fn to_int(roman: &str) -> Result<usize, &'static str> {
-    let mut skip: usize = 0;
+    let mut strip: &str = roman;
     let mut int: usize = 0;
-    for token in TABLE {
-        if skip > 0 {
-            skip -= 1;
-            continue;
-        }
-        match roman.find(token.0) {
-            Some(_) => {
+    for token in THOUSANDS {
+        match strip.strip_prefix(token.0) {
+            Some(new_strip) => {
+                strip = new_strip;
                 int += token.1;
-                skip = token.2;
+                break;
             }
             None => {
-                if token == *TABLE.last().unwrap() && int == 0 {
-                    return Err("Invalid input");
-                }
                 continue;
             }
         }
     }
-    Ok(int)
+    for token in HUNDREDS {
+        match strip.strip_prefix(token.0) {
+            Some(new_strip) => {
+                strip = new_strip;
+                int += token.1;
+                break;
+            }
+            None => {
+                continue;
+            }
+        }
+    }
+    for token in TENS {
+        match strip.strip_prefix(token.0) {
+            Some(new_strip) => {
+                strip = new_strip;
+                int += token.1;
+                break;
+            }
+            None => {
+                continue;
+            }
+        }
+    }
+    for token in UNITS {
+        match strip.strip_prefix(token.0) {
+            Some(new_strip) => {
+                strip = new_strip;
+                int += token.1;
+                break;
+            }
+            None => {
+                continue;
+            }
+        }
+    }
+    if strip.len() > 0 || int == 0 {
+        Err("Invalid input")
+    } else {
+        Ok(int)
+    }
 }
 
 #[cfg(test)]
@@ -79,6 +115,20 @@ mod tests {
     fn error_roman_to_int() {
         let roman = "XXXXXXMMMDCCCLXXXVIII";
         let int = to_int(roman);
-        assert_eq!(int, Ok(3_888));
+        assert_eq!(int, Err("Invalid input"));
+    }
+
+    #[test]
+    fn error_roman_to_int_again() {
+        let roman = "VIIII";
+        let int = to_int(roman);
+        assert_eq!(int, Err("Invalid input"));
+    }
+
+    #[test]
+    fn error_roman_to_int_retry() {
+        let roman = "IIV";
+        let int = to_int(roman);
+        assert_eq!(int, Err("Invalid input"));
     }
 }
